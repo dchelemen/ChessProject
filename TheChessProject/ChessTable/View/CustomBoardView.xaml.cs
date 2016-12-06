5e70c0c5b54645acde4574e1bc28ff5e4eb038b8
chDavid <chedav6733@gmail.com>
2016-12-06 15:18:54 +0100

    Added CustomBoardView for select how to start the game,

---
+++ TheChessProject/ChessTable/View/CustomBoardView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChessTable.View
+{
+    /// <summary>
+    /// Interaction logic for CustomBoardView.xaml
+    /// </summary>
+    public partial class CustomBoardView : Window
+    {
+        public CustomBoardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
