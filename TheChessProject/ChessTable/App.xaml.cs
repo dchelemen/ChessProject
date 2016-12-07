fb6f19a32f6b156e8e8aa9a2d4b64ad015fb5149
chDavid <chedav6733@gmail.com>
2016-12-07 11:16:35 +0100

    Added DelegateCommand and ViewModelBase

--- TheChessProject/ChessTable/App.xaml.cs
+++ TheChessProject/ChessTable/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using ChessTable.View;
+using ChessTable.ViewModels;
 
 namespace ChessTable
 {
@@ -15,11 +16,13 @@ namespace ChessTable
     public partial class App : Application
     {
         private MenuView mainView;
-        private ChessBoardView chessBoard;
+        private MenuViewModel menuViewModel;
 
         App()
         {
             mainView = new MenuView();
+            menuViewModel = new MenuViewModel();
+            mainView.DataContext = menuViewModel;
             mainView.Show();
         }
     }
---
+++ TheChessProject/ChessTable/ViewModels/MenuViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessTable.View;
+using System.Windows;
+
+namespace ChessTable.ViewModels
+{
+    public class MenuViewModel
+    {
+        public void onMenuStartBtnClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
